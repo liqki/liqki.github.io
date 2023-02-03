382817a36f5b540e1627d9d5b7e0ed4e6222e51b
BuildTools <leda.bonfert@gmail.com>
2023-02-03 18:55:07 +0100

    added functionality to add multiple projects with animation

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,9 @@ import { projects } from "../content/projectList";
 import { motion } from "framer-motion";
 import OpenNavMenu from "../components/OpenNavMenu";
 
+// creating custom motion component
+const MotionCard = motion(ProjectCard);
+
 function Projects({ setIsOpened }) {
   return (
     <>
@@ -12,17 +15,19 @@ function Projects({ setIsOpened }) {
       <div className="h-auto min-h-footer w-screen bg-dark flex justify-center items-center flex-col text-light">
         <h2 className="text-4xl mt-20">Projects</h2>
         <div className="lg:grid-cols-2 xl:grid-cols-3 grid-cols-1 w-[70vw] h-auto grid place-items-center">
-          {projects.map((project, i) => (
-            <motion.div
-              className="bg-dark text-light w-screen flex justify-center items-center flex-col"
-              key={i}
-              initial={{ translateX: -300, opacity: 0 }}
-              animate={{ translateX: 0, opacity: 1 }}
-              transition={{ duration: 0.2, delay: i * 0.2 }}
-            >
-              <ProjectCard name={project.name} id={project.id} img={project.images[0]} />
-            </motion.div>
-          ))}
+          {projects.map((project, i) => {
+            return (
+              <MotionCard
+                name={project.name}
+                id={project.id}
+                img={project.images[0]}
+                key={i}
+                initial={{ translateX: -300, opacity: 0 }}
+                animate={{ translateX: 0, opacity: 1 }}
+                transition={{ duration: 0.2, delay: i * 0.2 }}
+              />
+            );
+          })}
         </div>
       </div>
       <Footer />
